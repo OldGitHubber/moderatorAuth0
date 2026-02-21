a6e3630cbd83acfe71e94e83b8d9c155e76233d1
Tony <16122148+OldGitHubber@users.noreply.github.com>
2026-02-21 11:32:23 +0000

    Before removing the login functionality as it is now automated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const { auth, requiresAuth } = require('express-openid-connect'); // Authentication, Authorisation, Session management etc for Auth0 & OIDC
+const { auth, requiresAuth } = require('express-openid-connect'); // Authentication, ID token validation and session cookie generation
 const jwt = require('jsonwebtoken'); // Create & sign JWTs, Verify them and decode them. I use this in the check role function
 const dotenv = require('dotenv');
 const fs = require('fs');
@@ -10,86 +10,74 @@ dotenv.config();
 
 const app = express();
 
-const port = process.env.PORT || 3000;
-const AUTH0_CLAIMS_NAMESPACE = process.env.AUTH0_ROLES_NAMESPACE // Anything unique to avoid custom claims clashing with Auth0 defined claims
+const port = process.env.PORT
+const CUSTOM_CLAIMS_NAMESPACE = process.env.CUSTOM_CLAIMS_NAMESPACE // Anything unique to avoid custom claims clashing with Auth0 defined claims
 const AUDIENCE = process.env.AUDIENCE  // The name of the API in Auth0 to get the tokens from. Can be anything unique but once set in Auth0 APIit can't be changed
 
 const config = {
- authRequired: true,
- auth0Logout: true,
- secret: process.env.SECRET,
- baseURL: process.env.BASEURL,
- clientID: process.env.CLIENTID,
- issuerBaseURL: process.env.ISSUER,
- clientSecret: process.env.CLIENTSECRET,
- authorizationParams: {
-  response_type: 'code',
-  audience: AUDIENCE,  // API to call to get tokens
-  scope: 'openid profile email read:messages', // Scopes for ID token and access token. Added read:messages to show that we dn't always get what we ask for
-  //prompt: 'consent' // Always ask for consent
- }
-};
+  authRequired: true,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASEURL,
+  clientID: process.env.CLIENTID,
+  issuerBaseURL: process.env.ISSUER,
+  clientSecret: process.env.CLIENTSECRET,
+  authorizationParams: {
+    response_type: 'code',
+    audience: AUDIENCE,  // API to call to get tokens
+    scope: 'openid profile email', // Scopes for ID token and access token. openid to authenticate, profile for picture and email for email address
+    prompt: 'consent select_account'       // Always ask for consent. Always ask which account as opposed to silently signing in the same one every time google for example is selected
+  }
+}
 
-// Router with login, logout and callback is returned and provided to app.use to add to the middleware stack so routes are availabe as if defined in here
-// Basically does the whole of the OIDC authentication_code process
-app.use(auth(config));
+// Router with login, logout and callback is returned and provided to app.use to add to the middleware stack so routes are available as if defined in here
+app.use(auth(config))
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// This is some middleware to check the role is that of moderator. The middleware function is assigned to 
-// Pass in a role and see if it is in the accessToken. Return true or false
-// Could use either token as I added role to both of them
-const checkRole = (role) => {
- return (req, res, next) => {
-  const namespace = AUTH0_CLAIMS_NAMESPACE
-  const userRoles = req.oidc.user ? req.oidc.user[`${namespace}roles`] : []; // Extract the roles added to the token as claims
-
-  // Not so easy to get role from access token so probably better using idToken. However... need to install jsonwebtoken to decode access token to use it
-  const accessToken = req.oidc.accessToken
-  if (accessToken) {
-   const decodedToken = jwt.decode(accessToken.access_token); // Decode the access token
-   const permissions = decodedToken.permissions // Have a look at permissions. Use these as well as or instead of role
-   console.log('Permissions: ', permissions)
-   const roles = decodedToken[`${AUTH0_CLAIMS_NAMESPACE}roles`];  // Extract roles from the decoded token
-   console.log("Roles:", roles);
-   req.oidc.roles = roles              // I want to pass these back so I can output them so as this is middleware, add them to 
-   req.oidc.permissions = permissions  // the req object so they are passed along the middleware to the endpoint so I can send them back in the response
-  }
-
-  if (userRoles && userRoles.includes(role)) {  // Role we are looking for is there so go to next middleware. COuld check for permissions as well
-   return next();
-  } else {
-   res.status(403).json({ message: `You do not have permission to access this service.` }); // Role missing so block user and terminate middleware here
-  }
- };
-};
-
-
 app.get('/profile', requiresAuth(), (req, res) => {
- res.send(JSON.stringify(req.oidc.user)); // Return user profile
+  const userDetails = req.oidc.user
+  const roles = userDetails[`${CUSTOM_CLAIMS_NAMESPACE}roles`]
+
+  res.json({ userDetails: userDetails, role: roles[0] })   // Roles is an array as there may be more than one
 });
 
-// This endpoint needs a user to be authenticated, and the moderator role is in the token
-// If both functions succeed, let the user do whatever they came here to do. If either fails
-// they would have been rejected by the middleware
-// The roles and permissions are not in OIDC object so need soem middleware to retrieve them
-app.get('/moderator', requiresAuth(), checkRole('moderator'), (req, res) => {
- console.log(`In moderator!`)
- // If we get back here, the role and permissions must be there or the middleware would have returned http response
- res.json({
-  message: 'Moderator access approved',
-  idToken: req.oidc.idToken,
-  accessToken: req.oidc.accessToken,
-  roles: req.oidc.roles,
-  permissions: req.oidc.permissions
- })
+// This protected endpoint needs a user to be authenticated, and have the appropriate access. It also
+// receives the users role which needs to be administrator. 
+// If the user authenticates, and is an admin with admin:stuff permissions, let them in.
+// requiresAuth is OIDC middleware and will deal with the authentication. This is called before this route executes
+// If authentication fails, the request is terminated and response returned in middleware. If sucessful, we get into
+// this callback and check the role and permissions
+app.get('/moderator', requiresAuth(), (req, res) => {
+  console.log(`In moderator!`)
+
+  const accessToken = req.oidc.accessToken && req.oidc.accessToken.access_token // Remember the crummy javascript && is not logical but a selection
+  const claims = accessToken ? jwt.decode(accessToken) : {}
+
+  console.log('Decoded access token claims:', claims)
+
+  const permissions = claims.permissions || []; // There can be multiple permissions so returns an array
+  const roles = claims[`${CUSTOM_CLAIMS_NAMESPACE}roles`] || []; // claims is an array. One claim is the roles prefixed with the custom namespace added in Auth0
+
+  if (permissions.includes('access:all') && roles.includes('moderator')) {
+    res.json({
+      message: 'Moderator access approved',
+      idToken: req.oidc.idToken,
+      accessToken: req.oidc.accessToken,
+      roles: roles,
+      permissions: permissions
+    })
+  } else
+
+    res.status(403).json({ message: `You do not have permission to access this service.` }); // Role missing so block user and terminate middleware here
+
 });
 
 const httpsOptions = {
- key: fs.readFileSync('key.pem'),
- cert: fs.readFileSync('cert.pem'),
+  key: fs.readFileSync('key.pem', 'utf8'),
+  cert: fs.readFileSync('cert.pem', 'utf8'),
 };
 
 https.createServer(httpsOptions, app).listen(port, () => {
- console.log(`HTTPS Server is running on port ${port}`);
+  console.log(`HTTPS Server is running on port ${port}`);
 });
--- public/script.js
+++ public/script.js
@@ -1,126 +1,124 @@
+
+
 // Wait until html has been parsed and DOM created before trying to connect to stuff
 document.addEventListener('DOMContentLoaded', () => {
- const loginButton = document.getElementById('login');
- const logoutButton = document.getElementById('logout');
- const callApiButton = document.getElementById('call-api');
- const resultDiv = document.getElementById('result');
+//  const loginButton = document.getElementById('login');
+  const logoutButton = document.getElementById('logout');
+  const callApiButton = document.getElementById('call-api');
+  const resultDiv = document.getElementById('result');
 
- // For same origin, call the /login endpoint. This will redirect to Auth0
- loginButton.addEventListener('click', () => {
-  window.location.href = '/login';
- });
+  // For same origin, call the /login endpoint. This will redirect to Auth0
+  // loginButton.addEventListener('click', () => {
+  //   window.location.href = '/login';
+  // });
 
- // For same origin, call the /logout endpoint. This will redirect to Auth0
- logoutButton.addEventListener('click', () => {
-  window.location.href = '/logout';
- });
-
-
- // Add event listener to the call moderator button
- // Call the moderator endpoint
- // This will retrieve and display tokens if authorised
- // Or display not authorised message
- callApiButton.addEventListener('click', async () => {
-  try {
-   const response = await fetch('/moderator', {
-    credentials: 'same-origin'
-   });
-   if (response.status == 403) {
-    // throw new Error(await response.text()); 
-    const data = await response.json();
-    renderForbidden(data) // Only contains failure message
-   } else {
-    const data = await response.json(); // I return the tokens just to show them and decode for a look-see
-    renderTokens(data)                  // Write the tokens to the page
-   }
-  } catch (err) {
-   resultDiv.textContent = err.message;
-  }
- });
+  // For same origin, call the /logout endpoint. This will redirect to Auth0
+  logoutButton.addEventListener('click', () => {
+    window.location.href = '/logout';
+  });
 
 
- // On browser refresh, fetch /profile. If one is returned, 
- // the user must be authenticated so initialise the page 
- // If logged in, returns all profile data and sets appropriate buttons
- const checkAuth = async () => {
-  try {
-   const response = await fetch('/profile', {
-    credentials: 'same-origin'
-   });
-   if (response.ok) {
-    loginButton.style.display = 'none';    // Logged in so hide login button
-    logoutButton.style.display = 'block';  // Show logout
-    callApiButton.style.display = 'block'; // Show call moderator
-    const data = await response.json()     // Returns lots of data about the user based on the scope values
-    console.log('Response: ', data)        // Output in the browser console for info
-    renderData(data)                       // Format and write the data to the page
-   } else {
-    loginButton.style.display = 'block';   // Not logged in so just show login button
-    logoutButton.style.display = 'none';
-    callApiButton.style.display = 'none';
-   }
-  } catch (err) {
-   loginButton.style.display = 'block';    // If error, go to login state
-   logoutButton.style.display = 'none';
-   callApiButton.style.display = 'none';
-  }
- };
+  // Add event listener to the call moderator button
+  // Call the moderator endpoint
+  // This will retrieve and display tokens if authorised
+  // Or display not authorised message
+  callApiButton.addEventListener('click', async () => {
+    try {
+      const response = await fetch('/moderator', {
+        credentials: 'same-origin'
+      });
+      if (response.status == 403) {          // 403 Forbidden. Output the returned message
+        const data = await response.json();
+        renderForbidden(data)                // Only contains failure message
+      } else {
+        const data = await response.json(); // I return the tokens and data within them if moderator authorised
+        renderTokens(data)                  // Write the tokens etc to the page
+      }
+    } catch (err) {
+      resultDiv.textContent = err.message;
+    }
+  });
 
- checkAuth(); // Run once DOM is built
+
+  // On browser refresh, fetch /profile. 
+  // OIDS contacts Auth0 to check if the user has an authenticated session. If so, show the home page
+  // // and their profile returned fro Auth0
+  // If not, contact Auth0 for the login page
+  // Based on logged in state, appropriate buttons are displayed
+  const checkAuth = async () => {
+    try {
+      const response = await fetch('/profile', {
+        credentials: 'same-origin'
+      });
+      if (response.ok) {
+    //    loginButton.style.display = 'none';    // Logged in so hide login button
+        logoutButton.style.display = 'block';  // Show logout
+        callApiButton.style.display = 'block'; // Show call moderator
+        const data = await response.json()     // Returns lots of data about the user based on the scope values
+        console.log('Response: ', data)        // Output in the browser console for info
+        renderData(data)                       // Format and write the data to the page
+      }
+      // } else {
+      //   loginButton.style.display = 'block';   // Not logged in so just show login button
+      //   logoutButton.style.display = 'none';
+      //   callApiButton.style.display = 'none';
+      // }
+    } catch (err) {
+    //  loginButton.style.display = 'block';    // If error, go to login state
+      logoutButton.style.display = 'none';
+      callApiButton.style.display = 'none';
+    }
+  };
+
+  checkAuth(); // Run once DOM is built
 });
 
 
 // Render the profile data in a readable form
 function renderData(data) {
- const anonimisedEmail = data.email ? `user@${data.email.split('@')[1]}` : `Not available`
- const html = `<p>
- <b>Name:</b> ${data.name}<br>
- <b>Given Name:</b> ${data.given_name}<br>
- <b>Family Name:</b> ${data.family_name}<br>
- <b>Roles:</b> ${data['https://myapp.example.com/roles']}<br>
- <b>Email:</b> ${anonimisedEmail} ${data.email_verified ? '(verified)' : '(Not verified)'}<br>
- <b>Nickname:</b> User nickname. Typically start of email<br>
- <b>Updated at:</b> ${formatDate(data.updated_at)}<br>
- <b>sid:</b> (session id) ${data.sid}<br>
- <b>sub:</b> (subject) ${data.sub}<br>
- </p>` // Subject is a unique id across different systems
- document.getElementById('result').innerHTML = html
+  //const anonimisedEmail = data.userDetails.email ? `user@${data.userDetails.email.split('@')[1]}` : `Not available`
+  const email = data.userDetails.email || `Not available`
+  const html = `<p>
+    <span style="color:blue"><b>Role:</b> ${data.role}<br></span>
+    <b>Name:</b> ${data.userDetails.name}<br>
+    <b>Given Name:</b> ${data.userDetails.given_name}<br>
+    <b>Family Name:</b> ${data.userDetails.family_name}<br>
+    <b>Email:</b> ${email} ${data.email_verified ? '(verified)' : '(Not verified)'}<br>
+    <b>Nickname:</b> User nickname. Typically start of email<br>
+    <b>Updated at:</b> ${formatDate(data.userDetails.updated_at)}<br>
+    <b>sid:</b> (session id) ${data.userDetails.sid}<br>
+    <b>sub:</b> (subject) ${data.userDetails.sub}<br>
+    </p>` // Subject is a unique id across different systems
+  document.getElementById('result').innerHTML = html
 
- // Needed to load the picture seperately as it hadn't always downloaded by the time the html was rendered
- // so was intermittent. This makes sure it's loaded. Use its own div or the delay would render it after the text
- // if using the results div
- //const img = new Image();
-
- const img = document.createElement('img')
- img.src = data.picture;
- img.onload = function () {
-  const picDiv = document.getElementById('photo')
-  picDiv.appendChild(img)
- }
+  const img = document.createElement('img');
+  img.referrerPolicy = "no-referrer" // Do this before img.src. Overcomes Googles security policy which makes images intermittent
+  img.src = data.userDetails.picture;
+  document.getElementById('photo').appendChild(img);
 }
 
 // Date and time is stored in ISO format so format it for readability
 function formatDate(isoDate) {
- const date = new Date(isoDate);
+  const date = new Date(isoDate);
 
- // Options for formatting the date - e.g. '2025-02-23T14:04:52.473Z'
- const options = {
-  weekday: 'long',      // "Sunday"
-  year: 'numeric',      // "2025"
-  month: 'long',        // "February"
-  day: 'numeric',       // "23"
-  hour: '2-digit',      // "02"
-  minute: '2-digit',    // "04"
-  second: '2-digit',    // "52"
-  timeZoneName: 'short' // "GMT"
- };
+  // Options for formatting the date - e.g. '2025-02-23T14:04:52.473Z'
+  const options = {
+    weekday: 'long',      // "Sunday"
+    year: 'numeric',      // "2025"
+    month: 'long',        // "February"
+    day: 'numeric',       // "23"
+    hour: '2-digit',      // "02"
+    minute: '2-digit',    // "04"
+    second: '2-digit',    // "52"
+    timeZoneName: 'short' // "GMT"
+  };
 
- return date.toLocaleDateString('en-GB', options) // Convert to a readable format
+  return date.toLocaleDateString('en-GB', options) // Convert to a readable format
 }
 
 // Write the ID and access tokens to two text areas
 function renderTokens(data) {
- const html = `<p class="access-approved">${data.message}<p>
+  const html = `<p class="access-approved">${data.message}<p>
  <label><b>ID Token:</b></label><br>
  <textarea rows="20" cols="80">${data.idToken}</textarea><br><br>
 
@@ -130,22 +128,22 @@ function renderTokens(data) {
  <b>Expires in: </b>${formatSeconds(data.accessToken.expires_in)}:</label><br><br>
  <textarea rows="20" cols="80">${data.accessToken.access_token}</textarea>
  `
- document.getElementById('result').innerHTML = html
+  document.getElementById('result').innerHTML = html
 }
 
 function renderForbidden(data) {
- const html = `<p class="access-forbidden">${data.message}</p>`
- document.getElementById('result').innerHTML = html
+  const html = `<p class="access-forbidden">${data.message}</p>`
+  document.getElementById('result').innerHTML = html
 }
 
 // Token expiry is in seconds so format into a readable form
 function formatSeconds(seconds) {
- const days = Math.floor(seconds / (24 * 3600));
- seconds %= (24 * 3600);
- const hours = Math.floor(seconds / 3600);
- seconds %= 3600;
- const minutes = Math.floor(seconds / 60);
- seconds %= 60;
+  const days = Math.floor(seconds / (24 * 3600));
+  seconds %= (24 * 3600);
+  const hours = Math.floor(seconds / 3600);
+  seconds %= 3600;
+  const minutes = Math.floor(seconds / 60);
+  seconds %= 60;
 
- return `${days} day(s), ${hours} hour(s), ${minutes} minute(s), and ${seconds} second(s)`;
+  return `${days} day(s), ${hours} hour(s), ${minutes} minute(s), and ${seconds} second(s)`;
 }
